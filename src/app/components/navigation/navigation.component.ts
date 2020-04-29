809d6aad89086e19003255b2b917d2e52bec8e76
MaxvanderWeide <max.vanderweide@student.hu.nl>
2020-04-29 22:08:17 +0200

    Responsive design

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
-  selector: 'app-navigation',
-  templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss'],
+    selector: 'app-navigation',
+    templateUrl: './navigation.component.html',
+    styleUrls: ['./navigation.component.scss'],
 })
 
-export class NavigationComponent {
+export class NavigationComponent implements AfterViewInit {
+
+    @ViewChild('navBar', {read: ElementRef}) navBar: ElementRef;
+    protected isExtended = false;
+
+    public ngAfterViewInit(): void {
+        this.toggleNavbar();
+    }
+
+    public toggleNavbar(): void {
+        this.navBar.nativeElement.style.width = this.isExtended ? '250px' : '60px';
+        this.isExtended = !this.isExtended;
+    }
+
 }
