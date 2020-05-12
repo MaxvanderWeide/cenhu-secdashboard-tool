86734f86dc6e605ed5d0696b4d0d973707de9fc5
Nick Klomp <nick.klomp@student.hu.nl>
2020-05-12 15:52:58 +0200

    Changed Categories component to Academy component G-003

--- src/app/components/academy/academy.component.spec.ts
+++ src/app/components/academy/academy.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CategoriesComponent } from './categories.component';
+import { AcademyComponent } from './academy.component';
 
-describe('CategoriesComponent', () => {
-  let component: CategoriesComponent;
-  let fixture: ComponentFixture<CategoriesComponent>;
+describe('AcademyComponent', () => {
+  let component: AcademyComponent;
+  let fixture: ComponentFixture<AcademyComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CategoriesComponent ]
+      declarations: [ AcademyComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CategoriesComponent);
+    fixture = TestBed.createComponent(AcademyComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
