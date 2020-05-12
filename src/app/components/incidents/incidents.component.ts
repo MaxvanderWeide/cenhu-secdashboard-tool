9c7f38037ae64f61065fb71994ffb8fcaea871c0
milo <milo.voorhout@student.hu.nl>
2020-05-12 17:16:34 +0200

    Load in dummy data for messages G-002

--- src/app/components/incidents/incidents.component.ts
+++ src/app/components/incidents/incidents.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-incidents',
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./incidents.component.scss']
 })
 export class IncidentsComponent {
-
+  messages = [
+    {name: 'person Debora made a message'},
+    {name: 'person Opal made a message'},
+    {name: 'person Bean made a message'},
+    {name: 'person Osborne made a message'},
+    {name: 'person Debora made a message'},
+    {name: 'person Opal made a message'},
+    {name: 'person Bean made a message'},
+    {name: 'person Osborne made a message'},
+    {name: 'person Debora made a message'},
+    {name: 'person Opal made a message'},
+    {name: 'person Bean made a message'},
+    {name: 'person Osborne made a message'}
+  ];
 }
