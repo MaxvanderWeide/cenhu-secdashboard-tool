446b0e2e5a04730780ae29417931b41dde3c24b1
milo <milo.voorhout@student.hu.nl>
2020-05-14 17:39:03 +0200

    Color and text of the incidents boxes are now dynamic G-002

--- src/app/components/incident/incident.component.ts
+++ src/app/components/incident/incident.component.ts
@@ -28,7 +28,35 @@ export class IncidentComponent implements OnInit{
     {name: 'person Osborne made a message'}
   ];
 
+  severityIcon: string;
+  headerText: string;
+  severityClass: string;
+
   ngOnInit(): void {
     console.log(this.alertType);
+    this.checkAlertType(this.alertType);
+  }
+
+  checkAlertType(type: string): void {
+    switch (type) {
+      case 'high': {
+        this.severityIcon = 'fa-exclamation-circle';
+        this.headerText = 'High Security Risk Alerts';
+        this.severityClass = 'high-incident';
+        break;
+      }
+      case 'middle': {
+        this.severityIcon = 'fa-exclamation-triangle';
+        this.headerText = 'Medium Security Risk Alerts';
+        this.severityClass = 'middle-incident';
+        break;
+      }
+      case 'low': {
+        this.severityIcon = 'fa-external-link-square';
+        this.headerText = 'Low Security Risk Alerts';
+        this.severityClass = 'low-incident';
+        break;
+      }
+    }
   }
 }
