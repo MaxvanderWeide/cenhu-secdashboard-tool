7c5ff0cccb98a3842b3dc3033dde0d970a16b452
Terry <terry.zhou@student.hu.nl>
2020-05-14 15:54:10 +0200

    Change hamburger menu slide direction G-001

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -9,18 +9,19 @@ import {NavigationComponent} from '../navigation/navigation.component';
 })
 
 export class HeaderComponent {
-  @ViewChild('header', {read: ElementRef}) header: ElementRef;
+  @ViewChild('navMenu', {read: ElementRef}) navMenu: ElementRef;
 
   constructor(private navigationComponent: NavigationComponent) {
   }
 
   public toggleNav() {
-    this.navigationComponent.toggleNav();
-
-    if (this.navigationComponent.isRetracted) {
-      document.querySelector('.navbar-expand').classList.remove('navbar-expanded');
+    this.navigationComponent.toggleMobileNav();
+    if (this.navigationComponent.mobileRestracted) {
+      document.querySelector('.navbar-expand').classList.remove('mobile-navbar-expanded');
+      this.navMenu.nativeElement.querySelector('em').classList.replace('fa-times', 'fa-bars');
     } else {
-      document.querySelector('.navbar-expand').classList.add('navbar-expanded');
+      document.querySelector('.navbar-expand').classList.add('mobile-navbar-expanded');
+      this.navMenu.nativeElement.querySelector('em').classList.replace('fa-bars', 'fa-times');
     }
   }
 }
--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -10,8 +10,13 @@ import {Component} from '@angular/core';
 export class NavigationComponent {
 
   public isRetracted: boolean = true;
+  public mobileRestracted: boolean = true;
 
   public toggleNav() {
     this.isRetracted = !this.isRetracted;
+  }
+
+  public toggleMobileNav() {
+    this.mobileRestracted  = !this.mobileRestracted;
   }
 }
