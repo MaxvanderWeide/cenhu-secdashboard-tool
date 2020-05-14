76c9c2be29298fb6ce326f101a175815b5321835
Nick Klomp <nick.klomp@student.hu.nl>
2020-05-14 15:56:10 +0200

    CSV load in function G-003

---
+++ src/app/components/academy/CSVModel.ts
@@ -0,0 +1,13 @@
+export class CSVRecord {
+  public dateAssigned: any;
+  public dateStarted: any;
+  public dateCompleted: any;
+  public teams: any;
+  public progress: any;
+  public timeSpent: any;
+  public reviewScore: any;
+  public trainerReview: any;
+  public certificate: any;
+  public quizScore: any;
+  public quizAttempts: any;
+}
--- src/app/components/academy/academy.component.ts
+++ src/app/components/academy/academy.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {CSVRecord} from "./CSVModel";
 
 @Component({
   selector: 'app-academy',
@@ -6,4 +7,65 @@ import {Component} from '@angular/core';
   styleUrls: ['./academy.component.scss']
 })
 export class AcademyComponent {
+  public records: any[] = [];
+  @ViewChild('csvReader') csvReader: any;
+  uploadListener($event: any): void {
+    let text = [];
+    let files = $event.srcElement.files;
+    if (this.isValidCSVFile(files[0])) {
+      let input = $event.target;
+      let reader = new FileReader();
+      reader.readAsText(input.files[0]);
+      reader.onload = () => {
+        let csvData = reader.result;
+        let csvRecordsArray = (<string>csvData).split(/\r\n|\n/);
+        let headersRow = this.getHeaderArray(csvRecordsArray);
+        this.records = this.getDataRecordsArrayFromCSVFile(csvRecordsArray, headersRow.length);
+      };
+      reader.onerror = function () {
+        console.log('error is occured while reading file!');
+      };
+    } else {
+      alert("Please import valid .csv file.");
+      this.fileReset();
+    }
+  }
+  getDataRecordsArrayFromCSVFile(csvRecordsArray: any, headerLength: any) {
+    let csvArr = [];
+    for (let i = 1; i < csvRecordsArray.length; i++) {
+      let curruntRecord = (<string>csvRecordsArray[i]).split(',');
+      if (curruntRecord.length == headerLength) {
+        let csvRecord: CSVRecord = new CSVRecord();
+        csvRecord.dateAssigned = curruntRecord[0].trim();
+        csvRecord.dateStarted = curruntRecord[1].trim();
+        csvRecord.dateCompleted = curruntRecord[2].trim();
+        csvRecord.teams = curruntRecord[3].trim();
+        csvRecord.progress = curruntRecord[4].trim();
+        csvRecord.timeSpent = curruntRecord[5].trim();
+        csvRecord.reviewScore = curruntRecord[6].trim();
+        csvRecord.trainerReview = curruntRecord[7].trim();
+        csvRecord.certificate = curruntRecord[8].trim();
+        csvRecord.quizScore = curruntRecord[9].trim();
+        csvRecord.quizAttempts = curruntRecord[10].trim();
+        csvArr.push(csvRecord);
+      }
+    }
+    return csvArr;
+  }
+  isValidCSVFile(file: any) {
+    return file.name.endsWith(".csv");
+  }
+  getHeaderArray(csvRecordsArr: any) {
+    let headers = (<string>csvRecordsArr[0]).split(',');
+    let headerArray = [];
+    for (let j = 0; j < headers.length; j++) {
+      headerArray.push(headers[j]);
+    }
+    return headerArray;
+  }
+  fileReset() {
+    this.csvReader.nativeElement.value = "";
+    this.records = [];
+  }
+
 }
