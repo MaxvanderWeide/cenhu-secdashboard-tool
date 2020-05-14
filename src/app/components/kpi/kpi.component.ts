31d3fd27d5fcec3f8736699c5fae131b420ffdb4
Terry <terry.zhou@student.hu.nl>
2020-05-14 17:26:21 +0200

    Create KPI component G-004

---
+++ src/app/components/kpi/kpi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kpi',
+  templateUrl: './kpi.component.html',
+  styleUrls: ['./kpi.component.scss']
+})
+export class KpiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
