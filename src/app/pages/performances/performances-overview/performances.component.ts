de4d3cdc822832d42808a81e846ecd701d7c2917
Terry <terry.zhou@student.hu.nl>
2020-05-23 17:15:45 +0200

    Add title to charts; fix small styling issues G-004

--- src/app/pages/performances/performances-overview/performances.component.ts
+++ src/app/pages/performances/performances-overview/performances.component.ts
@@ -9,20 +9,29 @@ import {Label} from 'ng2-charts';
 })
 export class PerformancesOverviewComponent {
   public pieData: {
+    title: string,
     data: number[];
     labels: string[];
+    dataColors: string[]
   } = {
+    title: 'Taart is lekker',
     data: [300, 500, 100],
     labels: ['data1.1', 'data1.2', 'data1.3'],
+    dataColors: ['rgba(255,0,0,0.3)', 'rgba(0,255,0,0.3)', 'rgba(0,0,255,0.3)']
   };
 
   public lineData: {
+    title: string,
     data: ChartDataSets[],
-    labels: Label[]
+    labels: Label[],
+    dataColors: string[];
   } = {
+    title: 'Dit is een lijn',
     data: [
-      {data: [65, 59, 80, 81, 56, 55, 40, 52, 31, 23, 64, 31], label: 'Series A'}
+      {data: [65, 59, 80, 81, 56, 55, 40, 52, 31, 23, 64, 31], label: 'Series A'},
+      {data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], label: 'Series B'}
     ],
-    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    dataColors: ['blue', 'red']
   };
 }
