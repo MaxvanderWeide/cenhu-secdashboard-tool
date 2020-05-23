0ece26daeb8bf1dda66455c3a02d193c600bb4f1
MaxvanderWeide <max.vanderweide@student.hu.nl>
2020-05-23 17:25:05 +0200

    Changed case sensitivity G-006

--- .enforcement/commit-msg.sh
+++ .enforcement/commit-msg.sh
@@ -11,7 +11,7 @@ current_branch="$(git rev-parse --abbrev-ref HEAD)"
 commit_regex='(G-[0-9]+)'
 error_msg="Aborting commit. Your commit message is missing a ticket number"
 
-if ! grep -iqE "$commit_regex" "$1"; then
+if ! grep -qE "$commit_regex" "$1"; then
     echo "$error_msg" >&2
     exit 1
 fi
