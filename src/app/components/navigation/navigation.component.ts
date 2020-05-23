cfb137dbf1c3217ec00fe57fbebe819dcd8890c2
MaxvanderWeide <max.vanderweide@student.hu.nl>
2020-05-23 15:30:30 +0200

    Changed clik response G-006

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, HostListener} from '@angular/core';
 
 
 @Component({
@@ -8,7 +8,20 @@ import {Component} from '@angular/core';
 })
 
 export class NavigationComponent {
+
+  constructor(private eRef: ElementRef) {
+  }
+
+
   private retracted: boolean = true;
+
+  @HostListener('document:click', ['$event'])
+  // eslint-disable-next-line no-explicit-any
+  private toggleOnClick(event: any): void {
+    if (this.eRef.nativeElement.contains(event.target) || (!this.eRef.nativeElement.contains(event.target) && !this.isRetracted)) {
+      this.toggleRetracted('nav');
+    }
+  }
 
   public get isRetracted(): boolean {
     return this.retracted;
