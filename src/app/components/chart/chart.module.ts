9f383ca2302343a36be11d31d9aaaeee2cd56c66
milo <milo.voorhout@student.hu.nl>
2020-05-26 17:43:19 +0200

    Added a pie-chart G-003

---
+++ src/app/components/chart/chart.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {PieChartComponent} from '@components/chart/pie/pie.component';
+import {BarChartComponent} from '@components/chart/bar/bar.component';
+import {LineChartComponent} from '@components/chart/line/line.component';
+import {ChartsModule} from 'ng2-charts';
+
+
+
+@NgModule({
+  declarations: [
+    PieChartComponent,
+    BarChartComponent,
+    LineChartComponent
+  ],
+  exports: [
+    PieChartComponent,
+    BarChartComponent,
+    LineChartComponent
+  ],
+  imports: [
+    CommonModule,
+    ChartsModule
+  ]
+})
+export class ChartModule { }
