d0fe636ec1bc8c04f3ef2ed1fbe1f573347a937e
Nick Klomp <nick.klomp@student.hu.nl>
2020-05-28 17:32:12 +0200

    Added graphs to academy page G-003

--- src/app/pages/academy/academy-overview/academy-overview.component.ts
+++ src/app/pages/academy/academy-overview/academy-overview.component.ts
@@ -13,18 +13,29 @@ import SampleJson from 'src/assets/temp/convertcsv.json';
 export class AcademyOverviewComponent {
   records: Academy[] = [];
 
+  totalIssues: number = 0;
+
   avgHours: number = 0;
   avgProgress: number = 0;
+  totalProgress: number = 1;
   avgReviewScore: number = 0;
   avgTrainerReview: number = 0;
 
+  yAmount: number = 0;
   yPercentage: number = 0;
   nPercentage: number = 0;
 
   quizScore: number = 0;
   quizAttempt: number = 0;
 
-  public testPercentage: {
+  public certificatePercentage: {
+    title: string;
+    data: number[];
+    labels: string[];
+    dataColors: string[];
+  };
+
+  public progressPercentage: {
     title: string;
     data: number[];
     labels: string[];
@@ -55,6 +66,7 @@ export class AcademyOverviewComponent {
 
   calculateData(): void {
     let yAmount: number = 0;
+    let nAmount: number = 0;
 
     for (const record of this.records) {
       this.avgHours += record.timeSpent;
@@ -68,21 +80,36 @@ export class AcademyOverviewComponent {
       if (record.certificate === 'Y') {
         yAmount ++;
       }
+
+      if (record.certificate === 'N') {
+        nAmount ++;
+      }
     }
+
+    this.totalIssues = yAmount + nAmount;
 
     this.avgHours = Math.round(((this.avgHours / this.records.length) + Number.EPSILON) * 100) / 100;
     this.avgProgress = Math.round(((this.avgProgress / this.records.length) + Number.EPSILON) * 100) / 100;
+    this.totalProgress = Math.round(((1 - this.avgProgress) + Number.EPSILON) * 100) / 100;
     this.avgReviewScore = Math.round(((this.avgReviewScore / this.records.length) + Number.EPSILON) * 100) / 100;
     this.avgTrainerReview = Math.round(((this.avgTrainerReview / this.records.length) + Number.EPSILON) * 100) / 100;
 
+    this.yAmount = yAmount;
     this.yPercentage = Number(((yAmount / this.records.length) * 100).toFixed(2));
     this.nPercentage = Number((((this.records.length - yAmount) / this.records.length) * 100).toFixed(2));
 
 
-    this.testPercentage = {
-      title: 'How ',
+    this.certificatePercentage = {
+      title: 'Certificate',
       data: [this.yPercentage, this.nPercentage],
-      labels: ['Certifications', 'TopDesk notifications'],
+      labels: ['Certifications', 'Not certifications'],
+      dataColors: ['rgba(85,255,43,0.4)', 'rgba(250,247,55,0.4)']
+    };
+
+    this.progressPercentage = {
+      title: 'Progress',
+      data: [this.avgProgress, this.totalProgress],
+      labels: ['Progress done', 'In progress'],
       dataColors: ['rgba(85,255,43,0.4)', 'rgba(250,247,55,0.4)']
     };
   }
