9d97704789eecd252aa7cef3fb785832d79db543
MaxvanderWeide <max.vanderweide@student.hu.nl>
2020-05-28 18:12:12 +0200

    Disable eslnt on provider G-006

--- src/app/services/env.service.provider.ts
+++ src/app/services/env.service.provider.ts
@@ -1,30 +1,28 @@
+/* eslint-disable */
 import { EnvService } from './env.service';
 
 interface WindowEnv {
-  __env?: any; // eslint-disable-line @typescript-eslint/no-explicit-any
+  __env?: any;
 }
 
-export const EnvServiceFactory = () => { // eslint-disable-line @typescript-eslint/typedef @typescript-eslint/explicit-function-return-type
-  // Create env
+export const EnvServiceFactory = () => {
+
   const env: EnvService = new EnvService();
 
-  // Read environment variables from browser window
   const browserWindow: WindowEnv = (window || {}) as WindowEnv;
-  const browserWindowEnv: any = browserWindow.__env || {}; // eslint-disable-line @typescript-eslint/no-explicit-any
+  const browserWindowEnv: any = browserWindow.__env || {};
 
-  // Assign environment variables from browser window to env
-  // In the current implementation, properties from env.js overwrite defaults from the EnvService.
-  // If needed, a deep merge can be performed here to merge properties instead of overwriting them.
   for (const key in browserWindowEnv) {
-    if (browserWindowEnv.hasOwnProperty(key)) { // eslint-disable-line no-prototype-builtins
+    if (browserWindowEnv.hasOwnProperty(key)) {
       env[key] = browserWindow.__env[key];
     }
   }
   return env;
 };
 
-export const EnvServiceProvider = { // eslint-disable-line @typescript-eslint/typedef
+export const EnvServiceProvider = {
   provide: EnvService,
   useFactory: EnvServiceFactory,
   deps: [],
 };
+/* eslint-enable */
