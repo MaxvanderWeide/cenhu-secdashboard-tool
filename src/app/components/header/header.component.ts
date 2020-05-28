1f58ac6a4630896bc1bfd4287544efdb8e8bbed4
MaxvanderWeide <max.vanderweide@student.hu.nl>
2020-05-28 21:23:50 +0200

    Fixed static icon G-006

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, EventEmitter, Output, ViewChild} from '@angular/core';
+import {Component, ElementRef, Input, ViewChild} from '@angular/core';
 import {NavigationComponent} from '@app/components/navigation/navigation.component';
 
 @Component({
@@ -11,19 +11,16 @@ import {NavigationComponent} from '@app/components/navigation/navigation.compone
 export class HeaderComponent {
   @ViewChild('navMenu', {read: ElementRef}) navMenu: ElementRef;
 
-  @Output() rootAllowToggle = new EventEmitter();
-
-  constructor(private navigationComponent: NavigationComponent) {
-  }
+  @Input() rootSibling: NavigationComponent;
 
   public toggleNav(): void {
-    if (this.navigationComponent.retracted) {
+    if (this.rootSibling.retracted) {
       this.navMenu.nativeElement.querySelector('em').classList.replace('fa-bars', 'fa-times');
     } else {
       this.navMenu.nativeElement.querySelector('em').classList.replace('fa-times', 'fa-bars');
     }
 
     console.log('emit RootAllowToggle');
-    this.rootAllowToggle.emit(null);
+    this.rootSibling.toggleRetracted('header');
   }
 }
