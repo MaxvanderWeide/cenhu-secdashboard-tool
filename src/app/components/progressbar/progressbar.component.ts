8cdd91e772107b2a900f96fed7f21920dff0fe3c
wiers <wierish.bansie@student.hu.nl>
2020-06-02 17:25:01 +0200

    G-003 fixed eslint issues

--- src/app/components/progressbar/progressbar.component.ts
+++ src/app/components/progressbar/progressbar.component.ts
@@ -5,11 +5,5 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './progressbar.component.html',
   styleUrls: ['./progressbar.component.scss']
 })
-export class ProgressbarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ProgressbarComponent{
 }
