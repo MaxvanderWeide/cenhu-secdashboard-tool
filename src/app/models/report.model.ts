f545b449d022c241a5b00522aaeaa733d4b91a09
Stefanuh <stefan.jovanovic@live.nl>
2020-06-09 13:58:38 +0200

    Updated data stored G-007

--- src/app/models/report.model.ts
+++ src/app/models/report.model.ts
@@ -2,6 +2,7 @@ export interface Report {
   id: number;
   name: string;
   department: string;
+  subject: string;
   severity: string;
   description: string;
   date: string;
--- src/app/pages/report/report-overview/report-overview.component.ts
+++ src/app/pages/report/report-overview/report-overview.component.ts
@@ -2,14 +2,24 @@ import {Component, Input} from '@angular/core';
 import {Report} from '@models/report.model';
 import {DataService} from '@app/services/data.service';
 import {DressingService} from '@app/services/dressing.service';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-report-overview',
   templateUrl: './report-overview.component.html',
   styleUrls: ['./report-overview.component.scss']
 })
+
 export class ReportOverviewComponent {
   public reports: Report[];
+  public nameInput: string;
+  public subjectInput: string;
+  public severityInput: string;
+  public departmentInput: string;
+  public descriptionInput: string;
+  public id: number;
+  public dateTime: string = new Date().toLocaleString();
+  public reTime: string;
 
   constructor(private dataService: DataService, private dressingService: DressingService) {
     this.dataService.getReports().subscribe(
@@ -17,10 +27,24 @@ export class ReportOverviewComponent {
         this.reports = reports;
       },
       () => {
-        this.dressingService.message('Report data loading unsuccessful. Try again later.');
+        this.dressingService.message('Oops something went wrong, please try again later');
       }
     );
   }
-
+  onSubmit(reportForm: NgForm): void {
+    console.log(reportForm);
+    this.id = (this.reports.length + 1);
+    console.log(this.dateTime.replace(this.reTime, '$1 $2'));
+    // console.log(reportForm);
+    this.reports.push({
+      id: this.reports.length + 1,
+      name: this.nameInput,
+      department: this.departmentInput,
+      subject: this.subjectInput,
+      severity: this.severityInput,
+      description: this.descriptionInput,
+      date: this.dateTime.replace(this.reTime, '$1 $2'),
+      open: true
+    });
+  }
 }
-
