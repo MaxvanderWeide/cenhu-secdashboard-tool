33b14f8249c80cd03faf2769cd773a732175dd27
Stefanuh <stefan.jovanovic@live.nl>
2020-06-11 17:03:37 +0200

    Collapse animations added G-007

--- src/app/pages/report/report-overview/report-overview.component.ts
+++ src/app/pages/report/report-overview/report-overview.component.ts
@@ -12,14 +12,10 @@ import {NgForm} from '@angular/forms';
 
 export class ReportOverviewComponent {
   public reports: Report[];
-  public nameInput: string;
-  public departmentInput: string;
-  public subjectInput: string;
-  public severityInput: string;
-  public descriptionInput: string;
   public dateTime: string = new Date().toLocaleString();
   public reTime: string;
   public object: HTMLElement;
+  public departmentVal: boolean;
 
   constructor(private dataService: DataService, private dressingService: DressingService) {
     this.dataService.getReports().subscribe(
@@ -27,40 +23,28 @@ export class ReportOverviewComponent {
         this.reports = reports;
       },
       () => {
-        this.dressingService.message('Report data loading unsuccessful. Try again later.');
+        this.dressingService.message('Data loaded unsuccessfully. Please try again later.');
       }
     );
   }
 
   onSubmit(reportForm: NgForm): void {
-    // TODO : Remove dressing service (DressingService is a snackbar for client/server errors, not validation errors)
-    if (!this.nameInput) {
-      this.dressingService.message('Please fill in a name');
-    } else if (!this.departmentInput) {
-      this.dressingService.message('Please choose a department');
-    } else if (!this.subjectInput) {
-      this.dressingService.message('Please choose a subject');
-    } else if (!this.severityInput) {
-      this.dressingService.message('Please choose a level of severity');
-    } else if (!this.descriptionInput) {
-      this.dressingService.message('Please enter a description');
-    } else {
-      this.reports.push({
-        id: this.reports.length + 1,
-        name: this.nameInput,
-        department: this.departmentInput,
-        subject: this.subjectInput,
-        severity: this.severityInput,
-        description: this.descriptionInput,
-        date: this.dateTime.replace(this.reTime, '$1 $2'),
-        open: true
-      });
-    }
-    reportForm.reset();
+
+    this.departmentVal = typeof reportForm.value.departmentInput !== 'string';
+
+    this.reports.push({
+      id: this.reports.length + 1,
+      name: reportForm.value.nameInput,
+      department: reportForm.value.departmentInput,
+      subject: reportForm.value.subjectInput,
+      severity: reportForm.value.severityInput,
+      description: reportForm.value.descriptionInput,
+      date: this.dateTime.replace(this.reTime, '$1 $2'),
+      open: true
+    });
   }
 
   openUp(id: number): void {
-    // TODO : Add this functionality to HTML
     this.object = document.getElementById('content-' + id);
     if (this.object.classList.contains('active')) {
       this.object.classList.remove('active');
