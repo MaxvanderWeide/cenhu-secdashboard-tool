d88827c00cb143f8376c5994bdbc836bb6ed6765
MaxvanderWeide <max.vanderweide@student.hu.nl>
2020-06-13 10:06:29 +0200

    Added static 'systems used' G-014

--- src/app/components/datasec/datasec.component.ts
+++ src/app/components/datasec/datasec.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {BarChart} from '@models/barchart.model';
 
 @Component({
   selector: 'app-datasec',
@@ -6,5 +7,18 @@ import {Component} from '@angular/core';
   styleUrls: ['./datasec.component.scss']
 })
 export class DatasecComponent {
+  systemBarData: BarChart = {
+    title: 'Systems Used',
+    data: [
+      {data: [70, 0, 65], label: 'Windows'},
+      {data: [16.5, 0, 15], label: 'MacOS'},
+      {data: [3.5, 90, 15], label: 'Linux'},
+      {data: [10, 10, 5], label: 'Other'}
+    ],
+    labels: ['% Encrypted Systems', '% Unencrypted Systems', '% Total'],
+    dataColors: ['green', 'red'],
+    horizontal: false,
+    legend: true
+  };
 
 }
