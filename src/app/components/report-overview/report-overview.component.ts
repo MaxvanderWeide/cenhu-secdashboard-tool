fc556a2c51755cc0c0eba9b21c35c510fec41138
milo <milo.voorhout@student.hu.nl>
2020-06-23 18:15:50 +0200

    Fixed a couple of issues for filing a report G-007

--- src/app/components/report-overview/report-overview.component.ts
+++ src/app/components/report-overview/report-overview.component.ts
@@ -2,7 +2,8 @@ import {Component} from '@angular/core';
 import {Report} from '@models/report.model';
 import {DataService} from '@app/services/data.service';
 import {DressingService} from '@app/services/dressing.service';
-import {NgForm} from '@angular/forms';
+import {FormControl, NgForm, Validators} from '@angular/forms';
+import {Department} from '@models/department.model';
 
 @Component({
   selector: 'app-report-overview',
@@ -12,11 +13,15 @@ import {NgForm} from '@angular/forms';
 
 export class ReportOverviewComponent {
   public reports: Report[];
-  public dateTime: string = new Date().toLocaleString();
   public reTime: string;
   public departmentVal: boolean;
+  public departments: Department[];
 
   public nameInput: string;
+  public departmentControl = new FormControl('', Validators.required);
+  public subjectInput = new FormControl('', Validators.required);
+  public severityInput = new FormControl('', Validators.required);
+
 
   constructor(private dataService: DataService, private dressingService: DressingService) {
     this.dataService.getReports().subscribe(
@@ -27,18 +32,26 @@ export class ReportOverviewComponent {
         this.dressingService.message('Reports loading unsuccessfully. Please try again later.');
       }
     );
+    this.dataService.getDepartments().subscribe(
+      (departments: Department[]) => {
+        this.departments = departments;
+      },
+      () => {
+        this.dressingService.message('Department data loading unsuccessful. Try again later.');
+      }
+    );
   }
 
   onSubmit(reportForm: NgForm): void {
     this.departmentVal = typeof reportForm.value.departmentInput !== 'string';
-    if (this.nameInput !== undefined) {
+    if (reportForm.valid) {
       this.reports.push({
         name: reportForm.value.name,
         department: reportForm.value.departmentInput,
         subject: reportForm.value.subjectInput,
         severity: reportForm.value.severityInput,
         description: reportForm.value.descriptionInput,
-        date: this.dateTime.replace(this.reTime, '$1 $2'),
+        date: new Date().toLocaleString().replace(this.reTime, '$1 $2'),
         open: true
       });
     }
